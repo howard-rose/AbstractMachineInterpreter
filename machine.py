81cdd5a2cf0ffec191ef1c96e0adc605bcdfee0a
howard-rose <philip.anthon@gmail.com>
2025-03-21 00:49:17 +0800

    feat: implement Tape

--- machine.py
+++ machine.py
@@ -1,5 +1,11 @@
+from tape import *
+
+
 class Machine:
     def __init__(self, memory=None, tapes=None, states=None):
         self.memory = {} if memory is None else memory
         self.tapes = {} if tapes is None else tapes
         self.states = {} if states is None else states
+
+        self.input = self.tapes[next(iter(self.tapes))] if self.tapes else Tape()
+        self.output = Tape()
--- tape.py
+++ tape.py
@@ -1,2 +1,41 @@
+from collections import defaultdict
+
+
 class Tape:
-    pass
+    def __init__(self, default_char='#'):
+        if len(default_char) != 1:
+            raise Exception(f'Default character must be string of length 1, got "{default_char}"')
+        self.tape = defaultdict(lambda: default_char)
+        self.head = 0
+
+    def scan(self) -> str:
+        return self.tape[self.head]
+
+    def write(self, char: str):
+        if len(char) != 1:
+            raise Exception(f'Character must be string of length 1, got "{char}"')
+        self.tape[self.head] = char
+
+    def right(self):
+        self.head += 1
+
+    def left(self):
+        self.head -= 1
+
+
+class Tape2D(Tape):
+    def __init__(self):
+        super().__init__()
+        self.head = [0, 0] # x, y
+
+    def right(self):
+        self.head[0] += 1
+
+    def left(self):
+        self.head[0] -= 1
+
+    def up(self):
+        self.head[1] += 1
+
+    def down(self):
+        self.head[1] -= 1
