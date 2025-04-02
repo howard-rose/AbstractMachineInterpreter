153ddb20646f660519bef039fcd06c82bd567314
howard-rose <philip.anthon@gmail.com>
2025-04-03 04:00:57 +0800

    feat: backend routes

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-from fastapi import FastAPI
+from typing import Annotated
+from io import StringIO
+
+from fastapi import Body, FastAPI
 from parser import parse
 
 app = FastAPI()
 
+machine = None
+
 
 @app.get("/")
 async def root():
-    with open('sample_machines/sample5.txt', 'r') as machine_input:
-        machine = parse(machine_input)
-        while not machine.verdict:
-            machine.step()
-        return machine
-    # return {"message": "Hello World"}
+    # global machine
+    # with open('sample_machines/sample6.txt', 'r') as machine_input:
+    #     machine = parse(machine_input)
+    #     while not machine.verdict:
+    #         machine.step()
+    #     return machine
+    return {"message": "Hello World"}
+
+
+@app.get("/machine")
+async def get_machine():
+    return machine
+
+
+@app.post("/machine")
+async def load_machine(machine_str: Annotated[str, Body()]):
+    global machine
+    machine = parse(StringIO(machine_str))
+    return 'Machine loaded!'
+
+
+@app.post("/step")
+async def step():
+    if machine:
+        machine.step()
+        return 'Performed step!'
+    return 'Machine not loaded.'
+
+
+@app.post("/input")
+async def load_input(input: Annotated[str, Body()]):
+    if machine:
+        machine.reset(input)
+    return f'Got {input}!'
